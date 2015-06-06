7e8bf0947c869c6cb2f43db3f1e3a753e517352e
Ryan Oglesby <ryan.oglesby@manheim.com>
2015-06-06 18:22:20 -0500

    Add CORS Allow Headers

--- src/main/java/todo/middleware/CORSFilter.java
+++ src/main/java/todo/middleware/CORSFilter.java
@@ -16,6 +16,7 @@ public class CORSFilter implements Filter {
         HttpServletResponse response = (HttpServletResponse) servletResponse;
         response.setHeader("Access-Control-Allow-Origin", "*");
         response.setHeader("Access-Control-Allow-Methods", "OPTIONS, GET, POST, DELETE");
+        response.setHeader("Access-Control-Allow-Headers", "Content-Type");
         response.setHeader("Access-Control-Max-Age", "3600");
 
         filterChain.doFilter(servletRequest, response);
