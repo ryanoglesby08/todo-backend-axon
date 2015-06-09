539c1989fa27015ea6eb8ea07b192138ff1c8055
Ryan Oglesby <ryan.oglesby@manheim.com>
2015-06-09 14:23:35 -0400

    Finished unit tests for the CQRS domain - commands, aggregate, and events

--- src/test/java/todo/domain/ToDoItemAggregateTest.java
+++ src/test/java/todo/domain/ToDoItemAggregateTest.java
@@ -5,29 +5,43 @@ import org.axonframework.test.Fixtures;
 import org.junit.Before;
 import org.junit.Test;
 import todo.domain.command.CreateToDoItemCommand;
+import todo.domain.command.DeleteToDoItemCommand;
+import todo.domain.command.UpdateToDoItemCommand;
 import todo.domain.event.ToDoItemCreatedEvent;
+import todo.domain.event.ToDoItemDeletedEvent;
+import todo.domain.event.TodoItemUpdatedEvent;
 
 public class ToDoItemAggregateTest {
     private FixtureConfiguration fixture;
+    private ToDoItem todo;
 
     @Before
     public void setUp() throws Exception {
         fixture = Fixtures.newGivenWhenThenFixture(ToDoItemAggregate.class);
+        todo = new ToDoItem("do something", false, 2);
     }
 
     @Test
     public void creatingTodoItemsEmitsCreatedEvents() throws Exception {
-        ToDoItem todo = new ToDoItem("1", false, 2);
         fixture.given()
                 .when(new CreateToDoItemCommand("1", todo))
                 .expectEvents(new ToDoItemCreatedEvent("1", todo));
 
     }
 
-//    @Test
-//    public void markingToDoItemsAsCompleteEmitsCompletedEvents() throws Exception {
-//        fixture.given(new ToDoItemCreatedEvent("1", "Learn Axon"))
-//                .when(new MarkCompletedCommand("1"))
-//                .expectEvents(new ToDoItemCompletedEvent("1"));
-//    }
+    @Test
+    public void updatingTodoItemsEmitsUpdatedEvents() throws Exception {
+        ToDoItem updates = new ToDoItem(null, true, null);
+
+        fixture.given(new ToDoItemCreatedEvent("1", todo))
+                .when(new UpdateToDoItemCommand("1", updates))
+                .expectEvents(new TodoItemUpdatedEvent("1", updates));
+    }
+
+    @Test
+    public void deletingTodoItemsEmitsDeletedEvents() throws Exception {
+        fixture.given(new ToDoItemCreatedEvent("1", todo))
+                .when(new DeleteToDoItemCommand("1"))
+                .expectEvents(new ToDoItemDeletedEvent("1"));
+    }
 }
